eadefa6b4b398348f26c14c9801aa7f7d4e0bf5f
Trytomatie <trytomatie@gmail.com>
2024-08-15 12:30:29 +0200

    Added Height Fade Shader, Removed Faulty Code

--- Gone 4 Good/Assets/Scripts/AudioList.cs
+++ Gone 4 Good/Assets/Scripts/AudioList.cs
@@ -8,34 +8,3 @@ public class AudioList : ScriptableObject
     public float randomPitchRange = 0;
     public AudioClip[] audioClips;
 }
-
-public class TriggerLights : MonoBehaviour
-{
-    public GameObject[] candles;
-
-    public float timeIntervalCandles = 1.0f;
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (Time.time - timeIntervalCandles >= 0)
-        {
-            foreach (GameObject Candles in candles)
-            {
-                {
-                    Candles.SetActive(true);
-                }
-            }
-        }
-
-    }
-
-    private IEnumerator Itterator()
-    {
-        foreach (GameObject Candles in candles)
-        {
-            Candles.SetActive(true);
-            return yield new WaitForSeconds(timeIntervalCandles);
-        }
-        return null;
-    }
-}
